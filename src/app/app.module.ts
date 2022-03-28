1681f59a024cbe27116fdc8d327caf810b3556c8
BobbyTherio <bobby2theriault@gmail.com>
2022-03-28 10:20:53 -0600

    routing-project

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Cmp1Component } from './components/cmp1/cmp1.component';
+import { Cmp2Component } from './components/cmp2/cmp2.component';
+import { Cmp3Component } from './components/cmp3/cmp3.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    Cmp1Component,
+    Cmp2Component,
+    Cmp3Component,
+    NotfoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
